657840f2f399f6cce8e7fc87fa86958470452692
RavenJhon2002 <ravenbuhay25@gmail.com>
2026-03-08 13:11:27 +0800

    Fix media serving in production and persist uploads on Render

--- faltek/faltek/settings.py
+++ faltek/faltek/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = Path(os.environ.get("MEDIA_ROOT", BASE_DIR / "media"))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
