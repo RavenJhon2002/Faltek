6e114e67e7cd794f2ead0d92e7240531f2a4bc02
RavenJhon2002 <ravenbuhay25@gmail.com>
2026-03-08 13:50:19 +0800

    Fix media serving in production and persist uploads on Render

--- faltek/faltek/urls.py
+++ faltek/faltek/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,13 +9,8 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += [
-        re_path(
-            rf"^{settings.MEDIA_URL.lstrip('/')}(?P<path>.*)$",
-            serve,
-            {"document_root": settings.MEDIA_ROOT, "insecure": True},
-        ),
-    ]
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+    insecure=not settings.DEBUG,
+)
